531eb28217bc6845cf8eabdc3567a1edbd027f0c
Vuongnd22 <nguyenvuong8304@gmail.com>
2025-02-21 07:08:26 +0700

    SG6L-8

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -1,24 +1,34 @@
 
 package dal;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 
 public class DBContext {
     protected Connection connection;
     public DBContext()
     {
         try {
-            // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName= DBFinal";
-            String username = "sa";
-            String password = "sa";
+            Properties props = new Properties();
+            InputStream input = DBContext.class.getClassLoader().getResourceAsStream("config.properties");
+            
+            if (input == null) {
+                throw new IOException("Config file not found");
+            }
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException | SQLException ex) {
-            System.out.println(ex);
+        } catch (ClassNotFoundException | SQLException | IOException ex) {
+            System.out.println("Database connection error: " + ex.getMessage());
         }
     }
 }
